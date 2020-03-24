93f2146c6dfcb681662a9ec98380a5c1c8d9b7f3
Maozk <549595297@qq.com>
2020-03-24 15:44:01 +0800

    使用axios做ajax框架

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,43 +1,51 @@
 import Taro from '@tarojs/taro'
 import {baseUrl, noConsole} from "../config";
+import { axios } from 'taro-axios'
+import errorCode from './errorCode'
 
-export default (options = {method: 'GET',data: {} }) =>{
-  var data = {};
+const service = axios.create({
+    baseURL: baseUrl,
+    timeout: 30000 // 请求超时时间
+})
 
-  //输出请求日志
-  if (!noConsole){
-    console.log(
-      `${new Date().toLocaleDateString()}【M=${options.url}】P=${JSON.stringify(data)}`
-    );
-  }
-
-  return Taro.request({
-    url: baseUrl + options.url,
-    data: {
-      ...data,
-    },
-    header: {
-      'Content-Type': 'application/json',
-    },
-    method: options.method.toUpperCase(),
-  }).then(res => {
-    const {statusCode, data} = res;
-    if(statusCode >= 200 && statusCode < 300){
-      if(!noConsole){
-        console.log(
-          `${new Date().toLocaleString()}【 M=${options.url} 】【接口响应：】`,
-          res
-        )
-      }
-      return data;
-    }else {
-        Taro.showToast({
-          title: '遇到未知错误',
-          icon: 'none',
-          mask: true,
-        });
-      console.log(`网络请求状态码${statusCode}`);
-    }
-  });
-
+service.defaults.timeout = 30000
+// 返回其他状态吗
+service.defaults.validateStatus = function (status) {
+    return status >= 200 && status <= 500 // 默认的
 }
+// 跨域请求，允许保存cookie
+service.defaults.withCredentials = true
+
+// HTTPrequest拦截
+service.interceptors.request.use(config => {
+    const isToken = (config.headers || {}).isToken === false
+    let token =  Taro.getStorageSync('access_token');
+    if (token && !isToken) {
+        config.headers['Authorization'] = 'Bearer ' + token// token
+    }
+    return config
+}, error => {
+    return Promise.reject(error)
+})
+
+
+// HTTPresponse拦截
+service.interceptors.response.use(res => {
+    const status = Number(res.status) || 200
+    const message = res.data.msg || errorCode[status] || errorCode['default']
+    if (status === 401) {
+
+        return
+    }
+
+    if (status !== 200 || res.data.code === 1) {
+
+        return Promise.reject(new Error(message))
+    }
+
+    return res
+}, error => {
+    return Promise.reject(new Error(error))
+})
+
+export default service
